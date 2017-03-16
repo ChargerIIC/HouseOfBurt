6e00e2783a3d93afa412e263b2318df0bec88cb1
ChargerIIC <ChargerIIC@Yahoo.com>
2017-03-16 08:41:15 -0700

    Did it again
    I swear this is the last time I restart the template. The Microsoft
    Javascriptservices Yoeman generator was too good to pass up.

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-namespace HouseOfBurt
+namespace WebApplicationBasic
 {
     public class Program
     {
@@ -17,7 +16,6 @@ namespace HouseOfBurt
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
