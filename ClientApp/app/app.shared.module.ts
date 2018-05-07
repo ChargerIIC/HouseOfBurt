68522a0f9cb9d5a719238dce739af8720da54c32
VBurt <ChargerIIC@Yahoo.com>
2018-05-07 15:35:13 -0700

    Intial Sections
    These are the sections we had in v1. First task is to bring these back

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -4,19 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+//TODO: Remove
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { NewsComponent } from './components/news/news.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ApplicationsComponent } from './components/applications/applications.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+		FetchDataComponent,
+		NewsComponent,
+		HomeComponent,
+		ContactComponent,
+		ProductsComponent,
+		ApplicationsComponent
     ],
     imports: [
         CommonModule,
@@ -25,8 +35,11 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+			{ path: 'counter', component: CounterComponent },
+			{ path: 'applications', component: ApplicationsComponent },
+			{ path: 'contact', component: ContactComponent },
+			{ path: 'news', component: NewsComponent },
+			{ path: 'products', component: ProductsComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ ClientApp/app/components/news/news.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+	selector: 'news',
+	templateUrl: './news.component.html',
+	styleUrls: ['./news.component.css']
+})
+
+export class NewsComponent {
+}
